232122652bb06670b2cb271c4b12d57b7dc43a28
Matt Toensing <matthew.toensing@gmail.com>
2021-09-14 17:47:17 -0700

    Create test and method for searching parks by activitys

--- app/services/nps_service.rb
+++ app/services/nps_service.rb
@@ -6,4 +6,12 @@ class NpsService < BaseService
     end
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  def self.get_nps_by_activity(activity)
+    response = conn('https://developer.nps.gov/api/v1/activities/parks').get do |f|
+      f.params['api_key'] = ENV['nps_key']
+      f.params['q'] = activity
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
--- spec/services/nps_service_spec.rb
+++ spec/services/nps_service_spec.rb
@@ -19,4 +19,19 @@ require 'rails_helper'
        expect(park[:directionsInfo]).to be_a(String)
      end
    end
+
+   it 'can get national parks based off of activity query params', :vcr do
+      query = 'Fishing'
+
+      parks = NpsService.get_nps_by_activity(query)
+
+      parks[:data][0][:parks].each do |park|
+        expect(park).to have_key(:parkCode)
+        expect(park[:parkCode]).to be_a(String)
+        expect(park).to have_key(:fullName)
+        expect(park[:fullName]).to be_a(String)
+        expect(park).to have_key(:url)
+        expect(park[:url]).to be_a(String)
+      end
+   end
  end
