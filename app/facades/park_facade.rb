1f7f5decbfa21d9fc61fe50bd36819e29a1f3011
Matt Kragen <mattdkragen@gmail.com>
2021-09-16 18:13:56 -0700

    Test drive 2 more methods

--- app/facades/park_facade.rb
+++ app/facades/park_facade.rb
@@ -5,4 +5,18 @@ class ParkFacade
       Park.new(detail)
     end
   end
+  
+  def self.create_parks_by_activity(activity)
+    json = NpsService.get_nps_by_activity(activity)
+    json[:data][0][:parks].map do |detail|
+      Park.new(detail)
+    end
+  end
+  
+  def self.create_parks_by_park_code(park_code)
+    json = NpsService.get_nps_by_parkcode(park_code)
+    json[:data].map do |detail|
+      Park.new(detail)
+    end
+  end
 end
--- spec/facade/park_facade_spec.rb
+++ spec/facade/park_facade_spec.rb
@@ -10,5 +10,23 @@ RSpec.describe ParkFacade do
         expect(park).to be_a(Park)
       end
     end
+    
+    it 'can create parks based off of activity query params', :vcr do
+      parks = ParkFacade.create_parks_by_activity('Fishing')
+      expect(parks.count).to eq(140)
+
+      parks.each do |park|
+        expect(park).to be_a(Park)
+      end
+    end
+    
+    it 'can create parks based off of park code', :vcr do
+      parks = ParkFacade.create_parks_by_park_code('romo')
+      expect(parks.count).to eq(1)
+
+      parks.each do |park|
+        expect(park).to be_a(Park)
+      end
+    end
   end
 end
