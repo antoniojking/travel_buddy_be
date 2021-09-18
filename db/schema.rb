c86879b619afad67dbe3f12621cc392bf09168f7
Matt Toensing <matthew.toensing@gmail.com>
2021-09-18 16:22:12 -0700

    Change user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_16_000158) do
+ActiveRecord::Schema.define(version: 2021_09_18_231823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,11 +71,9 @@ ActiveRecord::Schema.define(version: 2021_09_16_000158) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "authorization_token"
+    t.string "token"
     t.string "refresh_token"
     t.string "email"
-    t.string "first_name"
-    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
