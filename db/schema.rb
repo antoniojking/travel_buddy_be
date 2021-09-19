e52e748ca37c657e1fcd1c9789631a1a6ca83830
Matt Toensing <matthew.toensing@gmail.com>
2021-09-19 09:55:52 -0700

    Add park name to trips

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_18_232239) do
+ActiveRecord::Schema.define(version: 2021_09_19_164953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2021_09_18_232239) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "park_name"
     t.index ["user_id"], name: "index_trips_on_user_id"
   end
 
