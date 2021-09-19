69c329f7b2639f938d1188c801e8c1a87ea4e568
Matt Toensing <matthew.toensing@gmail.com>
2021-09-19 14:47:33 -0700

    Remove nested params from users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,8 +4,8 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-    user = User.find_or_create_by(email: params[:user][:email])
-    user.update(spotify_id: params[:user][:spotify_id], token: params[:user][:token], refresh_token: params[:user][:refresh_token])
+    user = User.find_or_create_by(email: params[:email])
+    user.update(spotify_id: params[:spotify_id], token: params[:token], refresh_token: params[:refresh_token])
     render(json: UserSerializer.new(user))
   end
 
