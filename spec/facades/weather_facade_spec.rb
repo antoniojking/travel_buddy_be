1d07adf208b32745c7f58817428c856bc5b49ca3
Matt Toensing <matthew.toensing@gmail.com>
2021-09-19 09:21:58 -0700

    Make changes to test to reflect new cassette

--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe WeatherFacade do
   it 'can create weather info', :vcr do
       expected = {
         id: "Boulder",
-        current_conditions: "Mist",
-        current_temp: 86.0,
-        forecast:[{condition: "Patchy rain possible", date: "2021-09-18", max_temp: 85.5, min_temp: 53.4}, {condition: "Partly cloudy", date: "2021-09-19", max_temp: 82.6, min_temp: 53.6}]
+        current_conditions: "Sunny",
+        current_temp: 77.0,
+        forecast:[{condition: "Partly cloudy", date: "2021-09-19", max_temp: 84.6, min_temp: 52.9}, {condition: "Patchy rain possible", date: "2021-09-20", max_temp: 70.7, min_temp: 42.4}]
       }
 
       weather_info = WeatherFacade.weather_info('boulder', 2)
