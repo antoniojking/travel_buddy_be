7d96019dec6212ea4ac8d8a41e719ff72c01cbcb
Matt Toensing <matthew.toensing@gmail.com>
2021-09-19 12:21:34 -0700

    Remove validations for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,8 +11,5 @@ RSpec.describe User do
   describe 'validations' do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
-    it { should validate_presence_of(:spotify_id) }
-    it { should validate_presence_of(:token) }
-    it { should validate_presence_of(:refresh_token) }
   end
 end
