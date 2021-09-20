c87f64f5247e0db5723628a5b60f5e49aa15c309
Matt Toensing <matthew.toensing@gmail.com>
2021-09-20 16:05:51 -0700

    Create show, update and delete functionality for accommodations

--- app/controllers/api/v1/accommodations_controller.rb
+++ app/controllers/api/v1/accommodations_controller.rb
@@ -4,10 +4,25 @@ class Api::V1::AccommodationsController < ApplicationController
     render(json: AccommodationSerializer.new(trip.accommodations))
   end
 
+  def show
+    trip = Trip.find(params[:trip_id])
+    accommodation = Accommodation.find(params[:id])
+    render(json: AccommodationSerializer.new(accommodation))
+  end
+
   def create
     trip = Trip.find(params[:trip_id])
     accommodation = trip.accommodations.create!(accommodation_params)
     render(json: AccommodationSerializer.new(accommodation))
+  end
+
+  def update
+    render(json: AccommodationSerializer.new(Accommodation.update(params[:id], accommodation_params)))
+  end
+
+  def destroy
+    accommodation = Accommodation.find(params[:id])
+    accommodation.destroy
   end
 
   private
--- spec/requests/api/v1/accommodations_request_spec.rb
+++ spec/requests/api/v1/accommodations_request_spec.rb
@@ -2,28 +2,28 @@ require 'rails_helper'
 
 RSpec.describe 'accommodations api' do
   describe 'happy path' do
-  it 'can create a new accommodation' do
-    user = create(:user)
-    trip = create(:trip, user: user)
-    accommodation_params = {
-      name: 'Camp 4',
-      location: 'Yosemite Valley',
-      details: 'Pitch your test behind the large boulder'
-    }
-    headers = {"CONTENT_TYPE" => "application/json"}
+    it 'can create a new accommodation' do
+      user = create(:user)
+      trip = create(:trip, user: user)
+      accommodation_params = {
+        name: 'Camp 4',
+        location: 'Yosemite Valley',
+        details: 'Pitch your test behind the large boulder'
+      }
+      headers = {"CONTENT_TYPE" => "application/json"}
 
-    post "/api/v1/trips/#{trip.id}/accommodations", headers: headers, params: JSON.generate(accommodation_params)
+      post "/api/v1/trips/#{trip.id}/accommodations", headers: headers, params: JSON.generate(accommodation_params)
 
-    expect(response).to be_successful
+      expect(response).to be_successful
 
-    accommodation = Accommodation.last
+      accommodation = Accommodation.last
 
-    expect(accommodation.name).to eq(accommodation_params[:name])
-    expect(accommodation.location).to eq(accommodation_params[:location])
-    expect(accommodation.details).to eq(accommodation_params[:details])
-  end
+      expect(accommodation.name).to eq(accommodation_params[:name])
+      expect(accommodation.location).to eq(accommodation_params[:location])
+      expect(accommodation.details).to eq(accommodation_params[:details])
+    end
 
-  it 'can send a list of a trips accommodations' do
+    it 'can send a list of a trips accommodations' do
       user = create(:user)
       trip = create(:trip, user: user)
       accommodation1 = create(:accommodation, trip: trip)
@@ -44,6 +44,68 @@ RSpec.describe 'accommodations api' do
         expect(accommodation[:attributes]).to have_key(:location)
         expect(accommodation[:attributes]).to have_key(:details)
       end
+    end
+
+    it 'can send one accommodation for a trip' do
+      user = create(:user)
+      trip = create(:trip, user: user)
+      accommodation = create(:accommodation, trip: trip)
+
+      get "/api/v1/trips/#{trip.id}/accommodations/#{accommodation.id}"
+
+      expect(response).to be_successful
+
+      accommodation = JSON.parse(response.body, symbolize_names: true)
+
+      expect(accommodation).to have_key(:data)
+
+      expect(accommodation[:data]).to have_key(:id)
+      expect(accommodation[:data]).to have_key(:attributes)
+      expect(accommodation[:data][:attributes]).to have_key(:name)
+      expect(accommodation[:data][:attributes]).to have_key(:location)
+      expect(accommodation[:data][:attributes]).to have_key(:details)
+    end
+
+    it 'can send update an existing accommodation for a trip' do
+      user = create(:user)
+      trip = create(:trip, user: user)
+      accommodation = create(:accommodation, trip: trip)
+      previous_name = accommodation.name
+      accommodation_params = {
+        name: 'Yosemit Lodge'
+      }
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      patch "/api/v1/trips/#{trip.id}/accommodations/#{accommodation.id}", headers: headers, params: JSON.generate(accommodation_params)
+
+      expect(response).to be_successful
+
+
+      updated_accommodation = Accommodation.find(accommodation.id)
+      expect(updated_accommodation.name).to_not eq(previous_name)
+
+      accommodation = JSON.parse(response.body, symbolize_names: true)
+
+      expect(accommodation).to have_key(:data)
+
+      expect(accommodation[:data]).to have_key(:id)
+      expect(accommodation[:data]).to have_key(:attributes)
+      expect(accommodation[:data][:attributes]).to have_key(:name)
+      expect(accommodation[:data][:attributes]).to have_key(:location)
+      expect(accommodation[:data][:attributes]).to have_key(:details)
+    end
+
+    it 'can delete an existing accommodation for a trip' do
+      user = create(:user)
+      trip = create(:trip, user: user)
+      accommodation = create(:accommodation, trip: trip)
+      previous_name = accommodation.name
+
+      delete "/api/v1/trips/#{trip.id}/accommodations/#{accommodation.id}"
+
+      expect(response).to be_successful
+
+      expect{Accommodation.find(accommodation.id)}.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
 
@@ -67,4 +129,4 @@ RSpec.describe 'accommodations api' do
       expect(error).to have_key(:message)
     end
   end
-  end
+end
