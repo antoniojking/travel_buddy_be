8b07a8f82c4956f22b25c85294ce67a98ddbeb7e
Matt Toensing <matthew.toensing@gmail.com>
2021-09-19 18:41:45 -0700

    Add user id to trip serializer for FE

--- app/serializers/trip_serializer.rb
+++ app/serializers/trip_serializer.rb
@@ -1,6 +1,6 @@
 class TripSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :start_date, :end_date, :park_code, :park_name
+  attributes :name, :start_date, :end_date, :park_code, :park_name, :user_id
 
   attributes :checklists do |object|
     object.checklists
