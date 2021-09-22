f25c01da88a54455656947fd970a3090f94dfb57
Matt Toensing <matthew.toensing@gmail.com>
2021-09-22 15:50:47 -0700

    Add custom validation for friendships

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -5,4 +5,18 @@ RSpec.describe Friendship do
     it { should belong_to(:user) }
     it { should belong_to(:friend) }
   end
+
+  describe 'validations' do
+    describe '::already_friends' do
+      it 'validates that a friendship is unique' do
+        user1 = create(:user)
+        user2 = create(:user)
+
+
+        friendship = Friendship.create!(user_id: user1.id, friend_id: user2.id)
+
+        expect{Friendship.create!(user_id: user1.id, friend_id: user2.id)}.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+  end
 end
