d9eba90d8c7979d5bb79ce7c9acb7a308e8d0ef3
Matt Toensing <matthew.toensing@gmail.com>
2021-09-21 18:01:00 -0700

    Add update and destroy to check lsit items with happy and sad path testing

--- app/controllers/api/v1/checklist_items_controller.rb
+++ app/controllers/api/v1/checklist_items_controller.rb
@@ -6,7 +6,24 @@ class Api::V1::ChecklistItemsController < ApplicationController
 
   def create
     checklist = Checklist.find(params[:checklist_id])
-    checklist_item = ChecklistItem.create!(name: params[:name], user_id: params[:user_id], checklist_id: checklist.id)
+    checklist_item = ChecklistItem.create!(checklist_item_params)
     render(json: ChecklistItemSerializer.new(checklist_item))
+  end
+
+  def update
+    checklist = Checklist.find(params[:checklist_id])
+    checklist_item = ChecklistItem.update(params[:id], checklist_item_params)
+    render(json: ChecklistItemSerializer.new(checklist_item))
+  end
+
+  def destroy
+    checklist_item = ChecklistItem.find(params[:id])
+    checklist_item.destroy
+  end
+
+  private
+
+  def checklist_item_params
+    params.permit(:name, :checklist_id, :user_id)
   end
 end
--- spec/requests/api/v1/checklist_items_request_spec.rb
+++ spec/requests/api/v1/checklist_items_request_spec.rb
@@ -61,46 +61,41 @@ RSpec.describe 'checklist items api' do
       travel_buddy2 = TravelBuddy.create(user: user2, trip: trip)
       checklist = create(:checklist, trip: trip)
       checklist_item = ChecklistItem.create!(name: 'Tent', user: user, checklist: checklist)
+      previous_name = checklist_item.name
 
       checklist_item_params = {
         name: 'Sleeping Bag',
-        user_id: user.id
       }
+
       headers = {"CONTENT_TYPE" => "application/json"}
 
       patch "/api/v1/trips/#{trip.id}/checklists/#{checklist.id}/checklist_items/#{checklist_item.id}", headers: headers, params: JSON.generate(checklist_item_params)
 
       expect(response).to be_successful
 
-      checklist_item = ChecklistItem.last
+      found_checklist_item = ChecklistItem.find(checklist_item.id)
 
-      expect(checklist_item.name).to eq(checklist_item_params[:name])
-      expect(checklist_item.user_id).to eq(checklist_item_params[:user_id])
-      expect(checklist_item.checklist_id).to eq(checklist.id)
+      expect(found_checklist_item.name).to_not eq(previous_name)
+      expect(found_checklist_item.name).to eq('Sleeping Bag')
     end
 
-    it 'can create a new checklist item' do
+    it 'can delete an existing checklist item' do
       user = create(:user)
       trip = create(:trip, user: user)
       user2 = create(:user)
       travel_buddy1 = TravelBuddy.create(user: user, trip: trip)
       travel_buddy2 = TravelBuddy.create(user: user2, trip: trip)
       checklist = create(:checklist, trip: trip)
-      checklist_item_params = {
-        name: 'Tent',
-        user_id: user.id
-      }
-      headers = {"CONTENT_TYPE" => "application/json"}
+      checklist_item = ChecklistItem.create!(name: 'Tent', user: user, checklist: checklist)
 
-      post "/api/v1/trips/#{trip.id}/checklists/#{checklist.id}/checklist_items", headers: headers, params: JSON.generate(checklist_item_params)
+      expect(ChecklistItem.count).to eq(1)
+
+      delete "/api/v1/trips/#{trip.id}/checklists/#{checklist.id}/checklist_items/#{checklist_item.id}"
 
       expect(response).to be_successful
 
-      checklist_item = ChecklistItem.last
-
-      expect(checklist_item.name).to eq(checklist_item_params[:name])
-      expect(checklist_item.user_id).to eq(checklist_item_params[:user_id])
-      expect(checklist_item.checklist_id).to eq(checklist.id)
+      expect(ChecklistItem.count).to eq(0)
+      expect{ChecklistItem.find(checklist_item.id)}.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
 
@@ -121,6 +116,49 @@ RSpec.describe 'checklist items api' do
 
       expect(response).to_not be_successful
       expect(response.status).to eq(400)
+
+      error = JSON.parse(response.body, symbolize_names: true)
+
+      expect(error).to have_key(:message)
+    end
+
+    it 'will not update a check list item if id is incorrect' do
+      user = create(:user)
+      trip = create(:trip, user: user)
+      user2 = create(:user)
+      travel_buddy1 = TravelBuddy.create(user: user, trip: trip)
+      travel_buddy2 = TravelBuddy.create(user: user2, trip: trip)
+      checklist = create(:checklist, trip: trip)
+      checklist_item = ChecklistItem.create!(name: 'Tent', user: user, checklist: checklist)
+      checklist_item_params = {
+        name: 'Sleeping Bag'
+      }
+
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      patch "/api/v1/trips/#{trip.id}/checklists/#{checklist.id}/checklist_items/1329843", headers: headers, params: JSON.generate(checklist_item_params)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+
+      error = JSON.parse(response.body, symbolize_names: true)
+
+      expect(error).to have_key(:message)
+    end
+
+    it 'will not delete a check list item if id is incorrect' do
+      user = create(:user)
+      trip = create(:trip, user: user)
+      user2 = create(:user)
+      travel_buddy1 = TravelBuddy.create(user: user, trip: trip)
+      travel_buddy2 = TravelBuddy.create(user: user2, trip: trip)
+      checklist = create(:checklist, trip: trip)
+      checklist_item = ChecklistItem.create!(name: 'Tent', user: user, checklist: checklist)
+
+      delete "/api/v1/trips/#{trip.id}/checklists/#{checklist.id}/checklist_items/312432"
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
 
       error = JSON.parse(response.body, symbolize_names: true)
 
