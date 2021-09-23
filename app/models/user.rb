2d9a18d0b44dafe0db8c22cb509a1aa074f21dce
Matt Toensing <matthew.toensing@gmail.com>
2021-09-23 14:32:39 -0700

    Add depend destroy for model resources

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
 
-  has_many :friendships
-  has_many :friends, through: :friendships
-  has_many :travel_buddies
-  has_many :trips, through: :travel_buddies
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships, dependent: :destroy
+  has_many :travel_buddies, dependent: :destroy
+  has_many :trips, through: :travel_buddies, dependent: :destroy
 end
