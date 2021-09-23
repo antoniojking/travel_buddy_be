adede775af7701adf1431c48477ca476b3fd905b
Carina Sweet <carina.sweet06@gmail.com>
2021-09-23 13:02:13 -0600

    Add travel buddy validation

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -5,7 +5,7 @@ class Friendship < ApplicationRecord
   belongs_to :friend, foreign_key: :friend_id, class_name: 'User'
 
   def already_friends
-    friendships = Friendship.where(user: self.user_id, friend: self.friend_id)
+    friendships = Friendship.where(user_id: self.user_id, friend_id: self.friend_id)
 
     errors.add(:friend_email, 'Friendship already exists') if !friendships.empty?
   end
--- spec/models/travel_buddy_spec.rb
+++ spec/models/travel_buddy_spec.rb
@@ -5,4 +5,18 @@ RSpec.describe TravelBuddy do
     it { should belong_to(:trip) }
     it { should belong_to(:user) }
   end
+
+  describe 'validations' do
+    describe '::already_travel_buddies' do
+      it 'validates that a travel buddy can only be added once' do
+        user1 = create(:user)
+        user2 = create(:user)
+        trip = create(:trip, user: user1)
+
+        TravelBuddy.create!(trip_id: trip.id, user_id: user2.id)
+
+        expect{TravelBuddy.create!(trip_id: trip.id, user_id: user2.id)}.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+  end
 end
